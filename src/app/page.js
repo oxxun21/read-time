50b7fea207976b4052300db034547e064c8238db
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-21 15:44:11 +0900

    fix: 새로고침 오류

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import s from "./mainPage.module.css";
 import SearchSeaction from "@/components/mainPage/search/searchSeaction";
 import PostsSection from "@/components/mainPage/post/postsSection";

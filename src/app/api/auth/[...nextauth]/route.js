75d9d074524b9703d9cb953a16267fc158a0599c
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-22 22:14:45 +0900

    (#4) feat: signIn callback 추가

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,3 +1,4 @@
+import { connectDatabase } from "@/lib/helpers/db-util";
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 
@@ -8,6 +9,19 @@ const handler = NextAuth({
       clientSecret: process.env.KAKAO_CLIENT_SECRET,
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const client = await connectDatabase();
+      const db = client.db();
+      const usersCollection = db.collection("users");
+      const existingUser = await usersCollection.findOne({ email: user.email });
+      if (!existingUser) {
+        await usersCollection.insertOne(user);
+      }
+      client.close();
+      return true;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
   database: process.env.DATABASE_URL,
 });

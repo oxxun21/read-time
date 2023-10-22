8df56c733de734dad35769d95b490b752338379e
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-22 19:23:33 +0900

    (#2) feat: 로그인시 사용자 정보 추가

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -9,23 +9,7 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-    async session({ session, token }) {
-      session.user = token;
-      return session;
-    },
-  },
-  session: {
-    strategy: "jwt",
-    maxAge: 30 * 24 * 60 * 60,
-  },
-  pages: {
-    signIn: "/signin",
-  },
-  // database: process.env.DATABASE_URL,
+  database: process.env.DATABASE_URL,
 });
 
 export { handler as GET, handler as POST };
---
+++ src/components/main/user/userInfo.js
@@ -0,0 +1,27 @@
+"use client";
+import { useSession } from "next-auth/react";
+import React from "react";
+import Image from "next/image";
+import s from "./userInfo.module.css";
+
+export default function UserInfo() {
+  const { data: session } = useSession();
+
+  console.log(session);
+
+  return (
+    <>
+      {session ? (
+        <article className={s.userArticle}>
+          <Image
+            src={session.user.image}
+            alt={`${session.user.name}님의 프로필 이미지`}
+            width={30}
+            height={30}
+          />
+          <span>{session.user.name}님 안녕하세요!</span>
+        </article>
+      ) : null}
+    </>
+  );
+}

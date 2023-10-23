60d1b8fbc12604d935d49527a2f74a59a9060e78
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-23 18:12:15 +0900

    (#3) docs: timecheck 페이지 컴포넌트 생성

--- src/app/timecheck/page.js
+++ src/app/timecheck/page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import s from "./timeCheckPage.module.css";
+import Calender from "@/components/time/calender";
+import Timeform from "@/components/time/timeform";
 
 export default function TimeCheck() {
   return (
     <div className={s.contain}>
-      <h2>잠시만 기다려주세요</h2>
-      <p>기능 개발 중인 페이지예요!</p>
+      <Calender />
+      <Timeform />
     </div>
   );
 }

f70ba99bc6429afd9af42a50a34515053f76779a
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-23 15:49:50 +0900

    (#4) design: 로그아웃 모달 추가

--- src/components/layout/layoutNav.js
+++ src/components/layout/layoutNav.js
@@ -6,16 +6,9 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Modal from "../modal/modal";
 
 export default function LayoutNav() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [loginModalOpen, setLoginModalOpen] = useState(false);
+  const [logoutModalOpen, setLogoutModalOpen] = useState(false);
   const { data: session } = useSession();
-
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
 
   return (
     <nav className={s.nav}>
@@ -30,7 +23,7 @@ export default function LayoutNav() {
           <li>
             <button
               type="button"
-              onClick={() => signOut()}
+              onClick={() => setLogoutModalOpen(true)}
               className={s.button}
             >
               Sign out
@@ -38,16 +31,21 @@ export default function LayoutNav() {
           </li>
         ) : (
           <li>
-            <button type="button" onClick={openModal} className={s.button}>
+            <button
+              type="button"
+              onClick={() => {
+                setLoginModalOpen(true);
+              }}
+              className={s.button}
+            >
               Sign In
             </button>
           </li>
         )}
       </ul>
-      {isModalOpen && (
-        <Modal closeModal={closeModal}>
+      {loginModalOpen && (
+        <Modal closeModal={() => setLoginModalOpen(false)}>
           <strong className={s.loginStrong}>Login</strong>
-
           <button
             type="button"
             onClick={() => signIn("kakao")}
@@ -58,6 +56,27 @@ export default function LayoutNav() {
           <p className={s.loginP}>
             이메일도 동의해주셔야 정상적인 사용이 가능합니다ㅠ.ㅠ
           </p>
+        </Modal>
+      )}
+      {logoutModalOpen && (
+        <Modal closeModal={() => setLogoutModalOpen(false)}>
+          <p>로그아웃 하시겠습니까?</p>
+          <div className={s.logoutBtnDiv}>
+            <button
+              type="button"
+              onClick={() => setLogoutModalOpen(false)}
+              className={s.logoutBtn}
+            >
+              아니요
+            </button>
+            <button
+              type="button"
+              onClick={() => signOut()}
+              className={s.logoutBtn}
+            >
+              네
+            </button>
+          </div>
         </Modal>
       )}
     </nav>

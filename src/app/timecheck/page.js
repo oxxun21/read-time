3607224360b688acfa44d003bcd9effb1b7383b6
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-24 23:05:09 +0900

    og test

--- src/app/timecheck/page.js
+++ src/app/timecheck/page.js
@@ -2,14 +2,10 @@ import React from "react";
 import s from "./timeCheckPage.module.css";
 import Calender from "@/components/time/calender";
 import Timeform from "@/components/time/timeform";
-import { openGraphImage } from "./../shared-metadata";
 
 export const metadata = {
   title: "Read Time | TimeCheck",
   description: "오늘의 독서 기록을 남겨봐요.",
-  openGraph: {
-    ...openGraphImage,
-  },
 };
 
 export default function TimeCheck() {

9428f40c1f9d0bf862af615c389e1d882fa8c006
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-24 20:50:21 +0900

    (#13) docs: meta 데이터 생성

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,15 @@
 import s from "./mainPage.module.css";
 import SearchSection from "@/components/main/search/searchSection";
 import PostsSection from "@/components/main/post/postsSection";
+import { openGraphImage } from "./shared-metadata";
+
+export const metadata = {
+  title: "Read Time | Home",
+  description: "기억하고 싶은 책 속 문장을 적어봐요.",
+  openGraph: {
+    ...openGraphImage,
+  },
+};
 
 export default function Home() {
   return (

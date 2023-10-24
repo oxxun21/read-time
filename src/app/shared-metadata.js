e308539772af4eec4e7f4a6e56d291a153775bdc
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-24 22:40:55 +0900

    og 이미지 테스트

--- src/app/shared-metadata.js
+++ src/app/shared-metadata.js
@@ -1,5 +1,6 @@
 export const openGraphImage = {
   title: "Read Time",
   description: "기억하고 싶은 책 속 문장을 적어봐요.",
-  images: ["/public/og_img.png"],
+  // images: ["/public/og_img.png"],
+  images: ["../../public/og_img.png"],
 };

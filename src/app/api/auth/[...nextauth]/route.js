355a512bb3819b27dc6917b94ce003efcb965746
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-30 20:43:52 +0900

    chore: mongodb adapter 설치

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,8 +1,10 @@
 import { connectDatabase } from "@/lib/helpers/db-util";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
 
-const handler = NextAuth({
+export const authOptions = {
+  // adapter: MongoDBAdapter(clientPromise),
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID,
@@ -11,6 +13,7 @@ const handler = NextAuth({
   ],
   callbacks: {
     async signIn({ user, account, profile }) {
+      console.log(user);
       const client = await connectDatabase();
       const db = client.db();
       const usersCollection = db.collection("users");
@@ -21,9 +24,15 @@ const handler = NextAuth({
       client.close();
       return true;
     },
+    session: ({ session, token }) => ({
+      ...session,
+      id: token.sub,
+    }),
   },
   secret: process.env.NEXTAUTH_SECRET,
   database: process.env.DATABASE_URL,
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };

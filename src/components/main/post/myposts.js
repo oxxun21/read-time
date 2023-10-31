149380a557d100d5dbc149ca990dc510cb3bbd6b
Oh Ga Eun <dhrkdms4113@naver.com>
2023-10-31 16:18:17 +0900

    (#16) refactor: mtposts 데이터 패치 방식 변경

--- src/components/main/post/myposts.js
+++ src/components/main/post/myposts.js
@@ -1,27 +1,25 @@
 import React from "react";
 import s from "./postview.module.css";
 import Image from "next/image";
+import PostDeleteBtn from "./postdeletebtn";
 import { headers } from "next/headers";
-import MyPostDeleteBtn from "./mypostdeletebtn";
 
 async function dataFetch() {
   try {
     const host = headers().get("host");
     const protocal = process.env.NODE_ENV === "development" ? "http" : "https";
-    let res = await fetch(`${protocal}://${host}/api/postsview`, {
+    const response = await fetch(`${protocal}://${host}/api/postsview`, {
       cache: "no-store",
       headers: headers(),
     });
-    let data = await res.json();
+    const data = await response.json();
     return data;
   } catch (error) {
     console.error(error);
   }
 }
-
 export default async function Myposts() {
   const posts = await dataFetch();
-  console.log(posts);
 
   return (
     <>
@@ -33,17 +31,12 @@ export default async function Myposts() {
         <>
           {posts.map((post) => (
             <li key={post._id}>
-              <Image
-                src={post.thumbnail}
-                alt={post.title}
-                width={70}
-                height={100}
-              />
+              <Image src={post.thumbnail} alt={post.title} width={70} height={100} />
               <div>
                 <strong>{post.title}</strong>
                 <p>{post.sentence}</p>
               </div>
-              <MyPostDeleteBtn post={post} />
+              <PostDeleteBtn post={post} />
             </li>
           ))}
         </>

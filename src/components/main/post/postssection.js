732935b32b153467a6b99b6d5317a125a834d260
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-02 16:09:39 +0900

    (#22) feat: PostView Suspense 적용

--- src/components/main/post/postssection.js
+++ src/components/main/post/postssection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import s from "./postssection.module.css";
 import PostView from "./postview";
 
@@ -6,7 +6,9 @@ export default async function PostsSection() {
   return (
     <section className={s.postsSection}>
       <h3>Post</h3>
-      <PostView />
+      <Suspense fallback={<p className={s.loading}>게시글을 불러오는 중입니다...</p>}>
+        <PostView />
+      </Suspense>
     </section>
   );
 }

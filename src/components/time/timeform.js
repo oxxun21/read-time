7276b198c374c49762eb1a9eca0f8887910c1cf7
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-03 20:32:19 +0900

    refactor: 시간 등록 handleSubmit

--- src/components/time/timeform.js
+++ src/components/time/timeform.js
@@ -8,6 +8,29 @@ export default function Timeform({ value, record, setRecord }) {
   const minutesRef = useRef();
   const [isSubmit, setIsSubmit] = useState(false);
 
+  const fetchRecord = async (url, method, body) => {
+    try {
+      const response = await fetch(url, {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        setRecord(data);
+        alert(`${method === "POST" ? "기록" : "수정"} 완료!`);
+      }
+    } catch (error) {
+      console.error("데이터 저장 오류:", error);
+      alert("저장 중 오류가 발생했습니다.");
+    } finally {
+      setIsSubmit(false);
+    }
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const hoursValue = hoursRef.current.value;
@@ -26,52 +49,15 @@ export default function Timeform({ value, record, setRecord }) {
     setIsSubmit(true);
 
     if (!record || record.length === 0) {
-      try {
-        const response = await fetch(`/api/timerecord`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            date: moment(value).format("YYYY-MM-DD"),
-            time: formattedReadingTime,
-          }),
-        });
-        const data = await response.json();
-        console.log(data);
-        if (response.ok) {
-          setRecord(data);
-          alert("기록 완료!");
-        }
-      } catch (error) {
-        console.error("데이터 저장 오류:", error);
-        alert("저장 중 오류가 발생했습니다.");
-      } finally {
-        setIsSubmit(false);
-      }
+      fetchRecord(`/api/timerecord`, "POST", {
+        date: moment(value).format("YYYY-MM-DD"),
+        time: formattedReadingTime,
+      });
     } else {
-      try {
-        const response = await fetch(`/api/timerecord/edit`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            _id: record[0]._id,
-            time: formattedReadingTime,
-          }),
-        });
-        const data = await response.json();
-        if (response.ok) {
-          setRecord(data);
-          alert("수정 완료!");
-        }
-      } catch (e) {
-        console.error("데이터 저장 오류:", e);
-        alert("저장 중 오류가 발생했습니다.");
-      } finally {
-        setIsSubmit(false);
-      }
+      fetchRecord(`/api/timerecord/edit`, "PATCH", {
+        _id: record[0]._id,
+        time: formattedReadingTime,
+      });
     }
     hoursRef.current.value = "";
     minutesRef.current.value = "";
@@ -90,13 +76,12 @@ export default function Timeform({ value, record, setRecord }) {
         }),
       });
       const data = await response.json();
-      if (response.ok) {
+      if (response.status == 200) {
         setRecord(data);
         alert("삭제 완료!");
-      } else {
-        alert("삭제에 실패했습니다.");
       }
     } catch (e) {
+      alert("삭제에 실패했습니다.");
       console.error(e);
     } finally {
       setIsSubmit(false);

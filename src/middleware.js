0d8ac481f707e49e747770166f32fe352a9840f5
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-04 18:44:03 +0900

    fix: middleware token 변경

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export default async function middleware(request) {
-  const token = request.cookies.get("next-auth.session-token");
+  const token = request.cookies.get("__Secure-next-auth.session-token");
 
   if (!token) {
     return NextResponse.redirect(new URL("/", request.url));

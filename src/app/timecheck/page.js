40649360a046406ee8807303cc7bd5121c45b750
Oh Ga Eun <dhrkdms4113@naver.com>
2023-11-04 18:35:44 +0900

    docs: metadata 추가

--- src/app/timecheck/page.js
+++ src/app/timecheck/page.js
@@ -4,7 +4,7 @@ import Calender from "@/components/time/calender";
 
 export const metadata = {
   title: "Read Time | TimeCheck",
-  description: "오늘의 독서 기록을 남겨봐요.",
+  description: "오늘의 독서 시간을 남겨봐요.",
 };
 
 export default async function TimeCheck() {

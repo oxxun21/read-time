6849a8e3bbb9f37573a3d561e903fa36cb73584b
Oh Ga Eun <dhrkdms4113@naver.com>
2023-12-03 19:09:50 +0900

    build

--- src/components/layout/signinbtn.js
+++ src/components/layout/signinbtn.js
@@ -8,7 +8,7 @@ export default function SignInBtn() {
   const [loginModalOpen, setLoginModalOpen] = useState(false);
 
   const handleSignIn = async () => {
-    await signIn("kakao", { callbackUrl: "/", redirect: false });
+    await signIn("kakao", { callbackUrl: "/" });
   };
 
   return (

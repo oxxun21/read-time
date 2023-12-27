ed96d78e9dc51f90a3b21ec41662aa6095c18956
Oh Ga Eun <dhrkdms4113@naver.com>
2023-12-27 20:49:18 +0900

    build

--- src/components/main/post/postssection.js
+++ src/components/main/post/postssection.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React from "react";
 import s from "./postssection.module.css";
 import PostView from "./postview";
 
@@ -6,9 +6,9 @@ export default async function PostsSection() {
   return (
     <section className={s.postsSection}>
       <h3>Post</h3>
-      <Suspense fallback={<p className={s.loading}>게시글을 불러오는 중입니다...</p>}>
-        <PostView />
-      </Suspense>
+      {/* <Suspense fallback={<p className={s.loading}>게시글을 불러오는 중입니다...</p>}> */}
+      <PostView />
+      {/* </Suspense> */}
     </section>
   );
 }
